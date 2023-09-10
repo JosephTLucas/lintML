850940a3771827938e750deceb106483e6ee9c95
Joseph Lucas <joe@joetl.com>
2023-09-10 13:34:11 -0500

    remove circular import

--- lintML/lintML.py
+++ lintML/lintML.py
@@ -9,7 +9,6 @@ from lintML.observation import Observation
 import os
 from lintML.report import Report
 from lintML.utils import is_valid_directory
-from lintML.cli import run
 
 
 class lintML:
@@ -53,4 +52,23 @@ class lintML:
 
 
 if __name__ == "__main__":
-    run()
+    parser = argparse.ArgumentParser(
+        prog="lintML",
+        description="Linter for ML training code",
+        epilog="Have any rules we should add?",
+    )
+    parser.add_argument(
+        "dir",
+        type=is_valid_directory,
+        default=os.getcwd(),
+        nargs="?",
+        help="The target directory to lint",
+    )
+    parser.add_argument(
+        "--full-report", action="store_true", help="Generate a full report"
+    )
+    args = parser.parse_args()
+    m = lintML(indir=Path(args.dir).resolve())
+    loop = asyncio.get_event_loop()
+    creds, code_findings = loop.run_until_complete(m.get_observations())
+    print(m.generate_report(creds, code_findings, args.full_report))
